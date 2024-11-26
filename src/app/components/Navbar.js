7080f13079e323dde2a9339e2795f482d79d8c2c
Michael Muzzall <TheMichaelPerson.com>
2024-11-26 02:45:01 -0800

    Tag Component and JavaScript Practice

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,10 +1,12 @@
 import Button from './Button'
+import Tag from './Tag'
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-indigo-50 p-6 items-center sticky top-0 shadow-lg">
       <div className="logo bg-indigo-600 rounded-full p-6 hover:scale-105 transition-all cursor-pointer"></div>
       <div className="flex gap-x-4 items-center">
+        <Tag />
         <Button title="Home" href="/" />
         <Button title="About" href="about" />
       </div>
