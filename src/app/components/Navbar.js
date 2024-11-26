a7ea8182a01a1d204ea8eb370e5db739bd3d2876
Michael Muzzall <TheMichaelPerson.com>
2024-11-26 03:05:33 -0800

    Tag Component and JavaScript Practice

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,26 +3,26 @@ import Tag from './Tag'
 
 const navigationButtons = [
   {
+    title: 'About',
+    href: '/about',
+    isNew: true,
+  },
+  {
     title: 'Home',
     href: '/',
     isNew: false,
   },
-  {
-    title: 'About',
-    href: 'About',
-    isNew: true,
-  },
 ]
-const isNew = ''
+
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-indigo-50 p-6 items-center sticky top-0 shadow-lg">
       <div className="logo bg-indigo-600 rounded-full p-6 hover:scale-105 transition-all cursor-pointer"></div>
       <div className="flex gap-x-4 items-center">
-        {navigationButtons.map(({ href, title }) => (
+        {navigationButtons.map(({ href, title, isNew }) => (
           <>
             <Button href={href} classNameProp="flex text-xl">
-              {isNew && <Tag classNames="mr-2 nimate-pulse" />}
+              {isNew && <Tag classNames="mr-2 animate-pulse" />}
               {title}
             </Button>
           </>
