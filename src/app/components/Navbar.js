77752dd0dcb52360560edcda92b0f3d81900a599
Michael Muzzall <TheMichaelPerson.com>
2024-12-12 12:12:20 -0800

    Add Logo, Title, and Favicon

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,5 +1,6 @@
 import Button from './Button'
 import Tag from './Tag'
+import { CodeBracketIcon } from '@heroicons/react/24/solid'
 
 const navigationButtons = [
   {
@@ -17,7 +18,12 @@ const navigationButtons = [
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-indigo-50 p-6 items-center sticky top-0 shadow-lg">
-      <div className="logo bg-indigo-600 rounded-full p-6 hover:scale-105 transition-all cursor-pointer"></div>
+      <a
+        href="/"
+        className=" bg-stone-50 p-4 rounded-full  hover:scale-105 hover:bg-indigo-100 hover:text-blue-600 tansition-all cursor-pointer"
+      >
+        <CodeBracketIcon className="w-8 h-8 text-blue-500" />
+      </a>
       <div className="flex gap-x-4 items-center">
         {navigationButtons.map(({ href, title, isNew }) => (
           <>
